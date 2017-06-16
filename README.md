74e852a72a6e007d4823a0d71b48a28dfce60f77
minhnt58 <tienminh.uet@gmail.com>
2017-06-16 22:47:28 +0700

    generate database automatically

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# API for Chatbot UET
+API for Chatbot UET
+
+### Config database, page facebook token
+* Create new MySql database
+* Config database, page facebook token in `.env` 
+* Run `npm run database`
+
+### Run
+```
+npm start
+```
--- migrations/20170225004200_chatbot.js
+++ migrations/20170225004200_chatbot.js
@@ -3,10 +3,6 @@ exports.up = function (knex, Promise) {
         knex.schema.createTable('users', function (table) {
             table.increments('id').unsigned();
             table.varchar('fb_id', 200).unique();
-            table.varchar('first_name', 200);
-            table.varchar('last_name', 200);
-            table.varchar('gender', 200);
-            table.timestamp('updated_at').defaultTo(knex.fn.now());
 
             table.index('id');
             table.index('fb_id');
@@ -16,8 +12,6 @@ exports.up = function (knex, Promise) {
             table.increments('id').unsigned();
             table.varchar('lhs_fb_id', 200);
             table.varchar('rhs_fb_id', 200);
-
-            table.timestamp('updated_at').defaultTo(knex.fn.now());
 
             table.index('id');
         }),
